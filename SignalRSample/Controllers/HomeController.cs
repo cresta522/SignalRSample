2e5d1719b8e32c13f2d86d2afd3d1758ba304e19
cresta522 <cresta522@gmail.com>
2019-02-04 00:09:49 +0900

    test

--- SignalRSample/Controllers/HomeController.cs
+++ SignalRSample/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+  
 namespace SignalRSample.Controllers
 {
 	public class HomeController : Controller
