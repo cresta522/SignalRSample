43295c2396027c40fd271af4497b39d2bcf1af7e
cresta522 <cresta522@gmail.com>
2019-02-05 00:07:00 +0900

    signalRgit add .

---
+++ SignalRSample/SignalR/ProgressHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SignalRSample.SignalR
+{
+	[HubName("progress")]
+	public class ProgressHub : Hub
+	{
+		public int Count = 1;
+
+		public static void SendMessage(string message, int count)
+		{
+			var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+			hubContext.Clients.All.sendMessage(string.Format(message), count);
+
+		}
+
+		public void GetCountAndMessage(string message)
+		{
+			StartProgress();
+			Clients.Caller.sendMessage(string.Format(message), Count);
+		}
+
+		private void StartProgress()
+		{
+			Task.Run(() => {
+				for (int i = 0; i <= 100; i++) {
+					Thread.Sleep(100);
+					SendMessage("処理中...", i);
+				}
+				SendMessage("おわったで！", 100);
+			});
+		}
+		
+	}
+}
